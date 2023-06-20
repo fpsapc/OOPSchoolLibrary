cef59fd2f0cbeb9a667109a2b72df45b759b8d40
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-06-20 16:31:24 +0500

    Update person.rb

--- person.rb
+++ person.rb
@@ -8,13 +8,13 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
   private
 
   def of_age
     @age >= 18
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
   end
 end
