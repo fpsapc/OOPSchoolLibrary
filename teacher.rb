9446af478f582728b4575616e23f84705985440b
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-06-24 01:33:31 +0500

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative './person'
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(name, name = 'Unknown', parent_permission: true)
+  def initialize(age, book, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
