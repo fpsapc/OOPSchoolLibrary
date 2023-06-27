d374a66e00383725ccc0b112cca273eadacf18c3
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-06-27 17:43:13 +0500

    remove linter errors

--- app.rb
+++ app.rb
@@ -15,27 +15,59 @@ class App
   end
 
   def select_option(user_choice)
-    case user_choice
-    when '1'
-      @book_methods.list_all_books
-    when '2'
-      @person_methods.list_all_people
-    when '3'
-      @person_methods.create_person
-      Storage.save_date('Person', @person_methods.people_list)
-    when '4'
-      @book_methods.create_book
-      Storage.save_date('books', @book_methods.books_list)
-    when '5'
-      @rentals_list.create_rental
-      Storage.save_date('Rental', @rentals_list.rentals_list)
-    when '6'
-      @rentals_list.list_all_rentals
-    when '7'
-      exit
+    options = {
+      '1' => method(:list_all_books),
+      '2' => method(:list_all_people),
+      '3' => method(:create_person_and_save),
+      '4' => method(:create_book_and_save),
+      '5' => method(:create_rental_and_save),
+      '6' => method(:list_all_rentals),
+      '7' => method(:exit_program)
+    }
+
+    selected_option = options[user_choice]
+
+    if selected_option
+      selected_option.call
     else
       puts 'Invalid option, please try again!'
     end
+
+    show_menu
+  end
+
+  def list_all_books
+    @book_methods.list_all_books
+  end
+
+  def list_all_people
+    @person_methods.list_all_people
+  end
+
+  def create_person_and_save
+    @person_methods.create_person
+    Storage.save_data('Person', @person_methods.people_list)
+  end
+
+  def create_book_and_save
+    @book_methods.create_book
+    Storage.save_data('books', @book_methods.books_list)
+  end
+
+  def create_rental_and_save
+    @rentals_list.create_rental
+    Storage.save_data('Rental', @rentals_list.rentals_list)
+  end
+
+  def list_all_rentals
+    @rentals_list.list_all_rentals
+  end
+
+  def exit_program
+    exit
+  end
+
+  def show_menu
     @options.show_menu
   end
 end
