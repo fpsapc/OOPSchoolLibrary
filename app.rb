812167c6f2ea3183090621900d8c1d918aa430bb
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-06-27 07:45:02 +0500

    follow SRP rule
    Follow instructions and remove select option from main.rb and add it in app.rb

--- app.rb
+++ app.rb
@@ -113,4 +113,26 @@ class App
   def exit
     puts 'Thank you for using the app, see you later!'
   end
+
+  def select_option(user_choice)
+    options = {
+      '1' => :list_all_books,
+      '2' => :list_all_people,
+      '3' => :create_person,
+      '4' => :create_book,
+      '5' => :create_rental,
+      '6' => :list_all_rentals,
+      '7' => :exit
+    }
+
+    method = options[user_choice]
+    if method.nil?
+      puts 'Invalid option, please try again!'
+      puts
+      show_menu
+    else
+      send(method)
+    end
+  end
+
 end
