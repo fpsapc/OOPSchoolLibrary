aa4521943aa9bf0abd159097ef70a69c9b645ef4
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-06-27 17:31:24 +0500

    made required changes

--- app.rb
+++ app.rb
@@ -1,137 +1,41 @@
-require './student'
-require './teacher'
-require './book'
-require './rental'
+require_relative './book_methods'
+require_relative './person_methods'
+require_relative './rental_methods'
+require_relative './storage'
 
 class App
-  def initialize(parent)
-    @parent = parent
-    @books_list = []
-    @people_list = []
-    @rentals_list = []
-  end
-
-  def list_all_books
-    if @books_list.empty?
-      puts 'No record found! Add some books...'
-    else
-      puts 'Available books in the library'
-      @books_list.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
-    end
-    @parent.show_menu
-  end
-
-  def list_all_people
-    if @people_list.empty?
-      puts 'No record found! Add a person...'
-    else
-      puts 'All people in the library'
-      @people_list.each do |person|
-        puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-      end
-    end
-    @parent.show_menu
-  end
-
-  def create_person
-    print 'Do you want to create a student(1) or a teacher(2)? [Input the number]: '
-    person_role = gets.chomp
-    case person_role
-    when '1'
-      create_student
-    when '2'
-      create_teacher
-    else
-      puts 'Please add a valid input!'
-    end
-    @parent.show_menu
-  end
-
-  def create_student
-    print 'Age: '
-    age = gets.chomp.to_i
-    print 'Name: '
-    name = gets.chomp
-    print 'Has parent permission? [Y/N]: '
-    parent_permission = gets.chomp.downcase == 'y'
-    @people_list.push(Student.new(age, name, parent_permission: parent_permission))
-    puts
-    puts 'Person created successfuly'
-  end
-
-  def create_teacher
-    print 'Age: '
-    age = gets.chomp
-    print 'Name: '
-    name = gets.chomp
-    print 'Specialization: '
-    specialization = gets.chomp
-    @people_list.push(Teacher.new(age, specialization, name))
-    puts
-    puts 'Person created successfuly'
-  end
-
-  def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    @books_list.push(Book.new(title, author))
-    puts 'Book created successfully'
-    @parent.show_menu
-  end
-
-  def create_rental
-    puts 'Select a book from the following list by number'
-    @books_list.each_with_index { |book, index| puts "#{index}) Title: '#{book.title}', Author: #{book.author}" }
-    book_num = gets.chomp.to_i
-    puts
-    puts 'Select a person from the following list by number (not id)'
-    @people_list.each_with_index do |person, index|
-      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-    person_num = gets.chomp.to_i
-
-    print 'Date: '
-    date = gets.chomp
-    @rentals_list.push(Rental.new(date, @books_list[book_num], @people_list[person_num]))
-    puts 'Rental created successfully'
-    @parent.show_menu
-  end
-
-  def list_all_rentals
-    print 'ID of person: '
-    id = gets.chomp.to_i
-
-    puts 'Rentals:'
-    @rentals_list.each do |rental|
-      puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}" if rental.person.id == id
-    end
-    @parent.show_menu
-  end
-
-  def exit
-    puts 'Thank you for using the app, see you later!'
+  def initialize(options)
+    @options = options
+    @book_methods = BookMethods.new
+    @person_methods = PersonMethods.new
+    @rentals_list = RentalMethods.new(@book_methods, @person_methods)
+    @book_methods.books_list = Storage.load_data('books')
+    @person_methods.people_list = Storage.load_data('Person')
+    @rentals_list.rentals_list = Storage.load_data('Rental')
   end
 
   def select_option(user_choice)
-    options = {
-      '1' => :list_all_books,
-      '2' => :list_all_people,
-      '3' => :create_person,
-      '4' => :create_book,
-      '5' => :create_rental,
-      '6' => :list_all_rentals,
-      '7' => :exit
-    }
-
-    method = options[user_choice]
-    if method.nil?
-      puts 'Invalid option, please try again!'
-      puts
-      show_menu
+    case user_choice
+    when '1'
+      @book_methods.list_all_books
+    when '2'
+      @person_methods.list_all_people
+    when '3'
+      @person_methods.create_person
+      Storage.save_date('Person', @person_methods.people_list)
+    when '4'
+      @book_methods.create_book
+      Storage.save_date('books', @book_methods.books_list)
+    when '5'
+      @rentals_list.create_rental
+      Storage.save_date('Rental', @rentals_list.rentals_list)
+    when '6'
+      @rentals_list.list_all_rentals
+    when '7'
+      exit
     else
-      send(method)
+      puts 'Invalid option, please try again!'
     end
+    @options.show_menu
   end
 end
